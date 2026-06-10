5bf5a38eb6baa3420f208786b30e5617dc3effc9
tanisha <Tanisha.sirohi25@gmail.com>
2026-06-10 20:02:15 +0530

    /health endpoint added

--- src/main/java/com/tanisha/bfhl/controller/BfhlController.java
+++ src/main/java/com/tanisha/bfhl/controller/BfhlController.java
@@ -4,8 +4,12 @@ import com.tanisha.bfhl.dto.BfhlRequestDto;
 import com.tanisha.bfhl.dto.BfhlResponseDto;
 import com.tanisha.bfhl.service.BfhlService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/bfhl")
@@ -24,6 +28,16 @@ public class BfhlController {
     @PostMapping
     public ResponseEntity<BfhlResponseDto> processData(@Valid @RequestBody BfhlRequestDto request) {
         BfhlResponseDto response = bfhlService.processData(request);
+        return ResponseEntity.ok(response);
+
+    }
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, Object>> health() {
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "UP");
+        response.put("statusCode", HttpStatus.OK.value());
+
         return ResponseEntity.ok(response);
     }
 }
